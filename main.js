e9c12437832103f258f123cf45fdda0b3343b91a
Vihaan-Patel <92143558+Vihaan-Patel@users.noreply.github.com>
2022-04-12 15:51:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects"
